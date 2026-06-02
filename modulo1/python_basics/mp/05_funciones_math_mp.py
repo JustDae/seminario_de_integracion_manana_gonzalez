82f18ea7a95c141513461dde45101ee5e4a4a85d
hyxuu <hyxuu05@gmail.com>
2026-06-01 21:33:09 -0500

    Limpieza de estructura

--- modulo1/python_basics/mp/05_funciones_math_mp.py
+++
@@ -1,8 +0,0 @@
-import math
-
-tarifa_fraccionada = 0.3542
-distancia_km = 12.8
-print(math.sqrt(distancia_km))
-print(math.ceil(tarifa_fraccionada))
-print(math.floor(tarifa_fraccionada))
-print(round(distancia_km, 1))
